248b621067c9834b1f946cd7ffe0954cc1fce455
wzt_pro <wzt_pro@163.com>
2020-08-10 17:18:40 +0800

    springboot 日志

---
+++ springboot/springboot-03-logging/src/main/java/com/atguigu/springboot03/Springboot03LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.springboot03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03LoggingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot03LoggingApplication.class, args);
+    }
+
+}
---
+++ springboot/springboot-03-logging/src/test/java/com/atguigu/springboot03/Springboot03LoggingApplicationTests.java
@@ -0,0 +1,28 @@
+package com.atguigu.springboot03;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot03LoggingApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void contextLoads() {
+        logger.trace("trace");
+        logger.debug("debug");
+        //springboot 默认的是info级别的，没有指定就使用springboot默认规定的级别，
+        // 我们还可以通过配置文件进行设置springboot的日志级别
+        logger.info("info");
+        logger.warn("warn");
+        logger.error("error");
+    }
+
+}
+
