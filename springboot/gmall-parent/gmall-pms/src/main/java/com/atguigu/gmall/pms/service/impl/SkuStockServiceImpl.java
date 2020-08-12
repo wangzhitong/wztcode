89262cfb89929b086c8ad9cd3cb791658aee2d04
wzt_pro <wzt_pro@163.com>
2020-08-12 18:17:16 +0800

    提交pms模块entity、service、mapper

---
+++ springboot/gmall-parent/gmall-pms/src/main/java/com/atguigu/gmall/pms/service/impl/SkuStockServiceImpl.java
@@ -0,0 +1,19 @@
+package com.atguigu.gmall.pms.service.impl;
+
+import com.atguigu.gmall.pms.entity.SkuStock;
+import com.atguigu.gmall.pms.mapper.SkuStockMapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * sku的库存 服务实现类
+ * </p>
+ *
+ * @author Lfy
+ * @since 2020-08-11
+ */
+@Service
+public class SkuStockServiceImpl extends ServiceImpl<SkuStockMapper, SkuStock> implements SkuStockService {
+
+}
--- springboot/gmall-parent/gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
+++ springboot/gmall-parent/gmall-pms/src/test/java/com/atguigu/gmall/pms/GmallPmsApplicationTests.java
@@ -1,13 +1,25 @@
 package com.atguigu.gmall.pms;
 
-import org.junit.jupiter.api.Test;
+import com.atguigu.gmall.pms.entity.Product;
+import com.atguigu.gmall.pms.service.impl.ProductService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class GmallPmsApplicationTests {
+public class GmallPmsApplicationTests {
+
+    @Autowired
+    ProductService productService;
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
+        Product byId = productService.getById(1);
+        System.out.println(byId);
+
     }
 
 }
